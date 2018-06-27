d861bbffa31a0272681da111b1debde2217d5fc4
DATSEC <deepdonfrenki@gmail.com>
2018-06-27 13:41:49 +0300

    Starwels p2pool

--- p2pool/starwels/networks/starwels.py
+++ p2pool/starwels/networks/starwels.py
@@ -12,12 +12,12 @@ P2P_PORT = 8343
 ADDRESS_VERSION = 0
 RPC_PORT = 8342
 RPC_CHECK = defer.inlineCallbacks(lambda starwelsd: defer.returnValue(
-            (yield helper.check_genesis_block(starwelsd, '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f')) and
+            (yield helper.check_genesis_block(starwelsd, '000000003d69a915e9da53348c5c272978bb743442e3a6341c11061c125811a2')) and
             not (yield starwelsd.rpc_getinfo())['testnet']
         ))
 SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//210000
 POW_FUNC = data.hash256
-BLOCK_PERIOD = 600 # s
+BLOCK_PERIOD = 2 # s
 SYMBOL = 'MAI'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Starwels') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Starwels/') if platform.system() == 'Darwin' else os.path.expanduser('~/.starwels'), 'starwels.conf')
 BLOCK_EXPLORER_URL_PREFIX = 'https://blockchain.info/block/'
